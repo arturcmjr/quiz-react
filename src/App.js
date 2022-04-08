2df613c8a3a58d0ba6d2d72902043349232439a9
Artur Junior <arturcmjr@gmail.com>
2022-04-08 10:12:52 -0300

    app name and logo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 import Game from "./components/game/Game";
 import Menu from "./components/menu/Menu";
 import GameOver from "./components/game/GameOver";
-
+import logo from "./logo.svg";
 
 class App extends Component {
   constructor(props) {
@@ -18,11 +18,18 @@ class App extends Component {
 
   render() {
     return (
-      /* TODO: display app name and logo */
       <div className="bg-orange-300 w-screen h-screen flex justify-center items-center text-gray-700">
+        <div className="flex items-center absolute top-3 h-1/6">
+          <img src={logo} width="60px" className="mr-2" alt="app_logo"></img>
+          <span className="text-5xl text-white font-light">QUIZ</span>
+        </div>
+
         <div className="bg-white rounded overflow-hidden shadow-md md:w-1/2 sm:w-full my-8 mx-4 p-4 relative max-w-2xl min-h-[400px] w-[900px]">
           {this.renderContent()}
         </div>
+
+        {/* TODO: use correct link */}
+        <a href="https://www.google.com/" className="absolute bottom-3 text-orange-700 hover:text-orange-900">arju.dev</a>
       </div>
     );
   }
@@ -48,7 +55,7 @@ class App extends Component {
   }
 
   startGame() {
-    this.setState({ gameState: 1, gameResult: null});
+    this.setState({ gameState: 1, gameResult: null });
   }
 
   resetGame() {
